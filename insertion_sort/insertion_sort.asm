542ea5f9694e3e8907bc79884f630566e4ed6188
Boris <into.the.deepest.space@gmail.com>
2018-01-16 06:08:47 -0800

    add comments to insertion_sort.asm

--- insertion_sort/insertion_sort.asm
+++ insertion_sort/insertion_sort.asm
@@ -1,8 +1,19 @@
+;------------------------------------------------------------------------------
+; Executable file	:	insertion_sort
+; Version			:	0.0
+; Created date 		:	2018-01-16
+; Last update		:	2018-01-16
+; Author			:	Boris
+; Description		:	???
+;
+; Assemble			:	see makefile
+;------------------------------------------------------------------------------
+
 section .text
-	global _start
+	global _start		; declaring starting point
 
 _start:
 
-	mov al,0x00000001
-	mov bl,0x00000000
-	int 0x80
+	mov al,0x00000001	; setting syscallcode 'exit'
+	mov bl,0x00000000	; setting return code
+	int 0x80			; performing interrupt
