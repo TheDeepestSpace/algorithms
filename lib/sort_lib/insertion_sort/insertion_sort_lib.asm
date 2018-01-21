9f4b8f9486ce8a1de8938a7a7c384986229c5d40
Boris <into.the.deepest.space@gmail.com>
2018-01-20 16:17:15 -0800

    add insertion_sort_lib.asm for insertion_sort algorithm implementation

---
+++ lib/sort_lib/insertion_sort/insertion_sort_lib.asm
@@ -0,0 +1,16 @@
+;------------------------------------------------------------------------------
+; Object file	:	insertion_sort_lib.o
+; Version		:	0.0
+; Created date 	:	2018-01-20
+; Last update	:	2018-01-20
+; Author		:	Boris
+; Description	:	???
+;
+; Assemble		: see ../../makefile
+;------------------------------------------------------------------------------
+
+section .text
+	global insertion_sort
+
+insertion_sort:
+	xor ax,ax
