3bb1ffade5f6eb9b91e89d0ee7313b4703ad2739
Boris <into.the.deepest.space@gmail.com>
2018-01-22 02:58:13 -0800

    add insertion_sert call to insertion_sort.asm

--- insertion_sort/insertion_sort.asm
+++ insertion_sort/insertion_sort.asm
@@ -2,7 +2,7 @@
 ; Executable file	:	insertion_sort
 ; Version			:	0.0
 ; Created date 		:	2018-01-16
-; Last update		:	2018-01-16
+; Last update		:	2018-01-21
 ; Author			:	Boris
 ; Description		:	???
 ;
@@ -11,17 +11,38 @@
 
 extern print_time
 extern print_endl
+extern print_string
+extern insertion_sort
+
+section .data
+	string_to_print: dd 0x01fff000,0x1fc0,0x01f0,0x1c,0x06
+	string_to_print_len equ 0x05
 
 
 section .text
 	global _start					; declaring starting point
 
 _start:
+
+	mov ebx,0x0a					; specifying base 10 in ebx
+	mov esi,string_to_print			; moving string pointer to esi
+	mov ecx,string_to_print_len		; moving string length to ecx
+	call print_string				; executing print_string
+	call print_endl					; executing print_endl
+
 	call print_time					; executing print_time
 	call print_endl					; executing print_endl
 
+	call insertion_sort				; executing insertion sort
+
 	call print_time					; executing print_time [1]
 	call print_endl					; executing print_endl [1]
+
+	mov ebx,0x0a					; specifying base 10 in ebx
+	mov esi,string_to_print			; moving string pointer to esi
+	mov ecx,string_to_print_len		; moving string length to ecx
+	call print_string				; executing print_string
+	call print_endl					; executing print_endl
 
 	mov al,0x01						; setting syscall code 'exit'
 	mov bl,0x0						; setting return code
