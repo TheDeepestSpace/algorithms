2df48714a4da2e1bb45d2ac51f10f2dd1ee8077d
Boris <into.the.deepest.space@gmail.com>
2018-01-22 02:55:38 -0800

    implemented insertion_sort routine

--- lib/sort_lib/insertion_sort/insertion_sort_lib.asm
+++ lib/sort_lib/insertion_sort/insertion_sort_lib.asm
@@ -2,7 +2,7 @@
 ; Object file	:	insertion_sort_lib.o
 ; Version		:	0.0
 ; Created date 	:	2018-01-20
-; Last update	:	2018-01-20
+; Last update	:	2018-01-22
 ; Author		:	Boris
 ; Description	:	???
 ;
@@ -24,11 +24,39 @@ section .text
 
 insertion_sort:
 	push esi
+	push eax
+	push ebx
 	push ecx
+	push edx
+	push edi
 
+	mov ebx,0x01
+.loop_1:
+	cmp ebx,ecx
+	ja .done
+	mov edx,[esi+ebx*4]
+	dec ebx
+	mov eax,ebx
+	inc ebx
+.loop_2:
+	cmp eax,0xffffffff
+	je .after_1
+	cmp [esi+eax*4],edx
+	jbe .after_1
 
-
-
-	pop esi
+	mov edi,[esi+eax*4]
+	mov [esi+eax*4+4],edi
+	dec eax
+	jmp .loop_2
+.after_1:
+	mov [esi+eax*4+4],edx
+	inc ebx
+	jmp .loop_1
+.done:
+	pop edi
+	pop edx
 	pop ecx
+	pop ebx
+	pop eax
+	pop esi
 	ret
