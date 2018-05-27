455e015aa639bf33fcf046fae852f9119e6e3549
Boris <into.the.deepest.space@gmail.com>
2018-05-27 14:36:51 -0700

    emply check_sorted reoutine in insertion_sort tester; rebuild

--- insertion_sort/insertion_sort.asm
+++ insertion_sort/insertion_sort.asm
@@ -13,10 +13,17 @@ extern print_time
 extern print_endl
 extern print_string
 extern insertion_sort
+extern check_sorted
 
 section .data
 %include "string.txt"
 	string_to_print_len equ 0x2710
+
+	str_sorted: db 'sorted',10,13	; define 'sorted' string
+	str_sorted_len equ $-str_sorted	; define length of 'sorted' string
+
+	str_unsorted: db 'unsorted',10,13	; define 'unsorted' string
+	str_unsorted_len equ $-str_unsorted	; define length of 'unsorted' string
 
 
 section .text
@@ -43,6 +50,24 @@ _start:
 	mov ecx,string_to_print_len		; moving string length to ecx
 	call print_string				; executing print_string
 	call print_endl					; executing print_endl
+
+	call check_sorted				; make call to check_sorted routine
+
+	mov eax,0x04					; set sys_wraite syscall code
+	mov ebx,0x02					; set std_out file fr write
+	jnc .unsorted					; go to .unsorted if CF=0
+
+	mov ecx,str_sorted				; set output buffer to value of str_sorted
+	mov edx,str_sorted_len			; set length of output buffer to
+									; str_sorted_len
+	jmp .int						; go to .int
+.unsorted:
+	mov ecx,str_unsorted			; set output buffer to value of
+									; str_unsorted
+	mov edx,str_unsorted_len		; set length of output buffer to
+									; str_unsorted_len
+.int:
+	int 0x80						; perform interrupt
 
 	mov al,0x01						; setting syscall code 'exit'
 	mov bl,0x0						; setting return code
